ccddf5049c16600d00875ec44a4f1b7401f79289
SoHyeun Kwon <tnflower@naver.com>
2021-02-04 10:04:13 +0900

    image upload 테스트 페이지 기능 완료

--- lib/screens/storage_image.dart
+++ lib/screens/storage_image.dart
@@ -21,6 +21,9 @@ class CloudStorageDemoState extends State<CloudStorageDemo> {
   User _user;
   FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
   String _profileImageURL = "";
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  final picker = ImagePicker();
 
   @override
   void initState() {
@@ -35,6 +38,7 @@ class CloudStorageDemoState extends State<CloudStorageDemo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(title: Text("Cloud Storage Demo")),
       body: Center(
         child: Column(
@@ -53,13 +57,13 @@ class CloudStorageDemoState extends State<CloudStorageDemo> {
                 RaisedButton(
                   child: Text("Gallery"),
                   onPressed: () {
-                    _uploadImageToStorage(ImageSource.gallery);
+                    handleUploadType('gallery');
                   },
                 ),
                 RaisedButton(
                   child: Text("Camera"),
                   onPressed: () {
-                    _uploadImageToStorage(ImageSource.camera);
+                    handleUploadType('camera');
                   },
                 )
               ],
@@ -83,23 +87,44 @@ class CloudStorageDemoState extends State<CloudStorageDemo> {
     );
   }
 
-  void _uploadImageToStorage(ImageSource source) async {
-    File image = await ImagePicker.pickImage(source: source);
+  void handleUploadType(String type) async {
+    PickedFile file;
 
-    if (image == null) return;
+    if (type == 'gallery') {
+      file = await ImagePicker().getImage(source: ImageSource.gallery);
+    } else {
+      file = await ImagePicker().getImage(source: ImageSource.camera);
+    }
+
+    if (file == null) return;
     setState(() {
-      _image = image;
+      _image = File(file.path);
     });
 
     // 프로필 사진을 업로드할 경로와 파일명을 정의. 사용자의 uid를 이용하여 파일명의 중복 가능성 제거
+    String newFileName = _user.email.split('@')[0] +
+        DateTime.now().millisecondsSinceEpoch.toString();
+
     Reference storageReference =
-        _firebaseStorage.ref().child("coffeebaseimage/${'test1'}");
+        _firebaseStorage.ref().child("coffeebaseimage/$newFileName");
+
+    _scaffoldKey.currentState
+      ..hideCurrentSnackBar()
+      ..showSnackBar(SnackBar(
+        duration: Duration(seconds: 10),
+        content: Row(
+          children: <Widget>[
+            CircularProgressIndicator(),
+            Text("uploading file...")
+          ],
+        ),
+      ));
 
     // 파일 업로드
     UploadTask storageUploadTask = storageReference.putFile(_image);
 
     // 파일 업로드 완료까지 대기
-    await storageUploadTask.whenComplete(() => null);
+    await storageUploadTask.whenComplete(null);
 
     // 업로드한 사진의 URL 획득
     String downloadURL = await storageReference.getDownloadURL();
@@ -107,6 +132,7 @@ class CloudStorageDemoState extends State<CloudStorageDemo> {
     // 업로드된 사진의 URL을 페이지에 반영
     setState(() {
       _profileImageURL = downloadURL;
+      _scaffoldKey.currentState.hideCurrentSnackBar();
     });
   }
 }
